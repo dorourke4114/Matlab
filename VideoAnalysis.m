f86b7f3988eb40fc0af00889374f9a196e1ca15b
Daniel O'Rourke <dorourke4114@gmail.com>
2017-08-09 17:31:48 -0400

    Update

--- VideoAnalysis.m
+++ VideoAnalysis.m
@@ -16,7 +16,7 @@ green_limit = 245;
 red_limit   = 245;
 dark_limit  = 230;
 
-circle_size = [5,100];
+circle_size = [1,5];
 
 
   
@@ -26,7 +26,7 @@ circle_size = [5,100];
   end
   
   %Determine if file or directory
-  [a,b]=strtok(in,'.');
+  [~,b]=strtok(in,'.');
   
   if isempty(b)
     %input might be a directory
@@ -35,46 +35,49 @@ circle_size = [5,100];
       
     else
       %error, invalid directory
+    end
   else
     %input is a filename
     
     v = VideoReader(in);
     point1=[];
     point2=[];
-    
+    framenumber=0;
     while hasFrame(v)
+        framenumber=framenumber+1;
+        sprintf('Processing frame %d',framenumber);
         %get frame
         img = readFrame(v);
         %split image
         r=img(:,:,1);g=img(:,:,2);b=img(:,:,3);
         %find green
-        gmask = and(g>green_limit,r<dark_limit,b<dark_limit);
+        gmask = g>green_limit & r<dark_limit & b<dark_limit;
         %find red
-        rmask = and(g<dark_limit,r>red_limit,b<dark_limit);
+        rmask = g<dark_limit & r>red_limit & b<dark_limit;
         %set red and green to white
         g(gmask)=254;r(gmask)=254;b(gmask)=254;
         g(rmask)=254;r(rmask)=254;b(rmask)=254;
         %sum red and green
         lmask=gmask+rmask;
         %make all not red or green black
-        g(!lmask)=0;r(!lmask)=0;b(!lmask)=0;
+        g(~lmask)=0;r(~lmask)=0;b(~lmask)=0;
         newimg=cat(3,r,g,b);
-        [centers,radii,metric]=imfindcircles(newimg,circle_size);
+        [centers,~,~]=imfindcircles(newimg,circle_size);
         if isempty(centers)
           %error, no points found
         else
-          [a,b]=size(centers);
+          [a,~]=size(centers);
           switch a
             case 1
               %error only 1 point found
             case 2
-              point1=[point1;centers(1,:)];
-              point2=[point2;centers(2,:)];
+              point1=[point1;centers(1,:)]; %#ok<*AGROW>
+              point2=[point2;centers(2,:)]; % Cannot preallocate video size
             
           end
         end
     end
   
-  
-  
+  end
+out={point1,point2};
 end
